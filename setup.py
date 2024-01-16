39ffc5deca6418e18ea9d1c90db841c92266f653
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2024-01-16 23:04:34 +0100

    moved project

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-)
